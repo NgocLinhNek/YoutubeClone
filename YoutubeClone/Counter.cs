dc8b11820ba0a93138b46be63522a06cf1aee3b1
NgocLinhNek <khongaibiet540@gmail.com>
2023-02-02 12:41:05 +0700

    setup template

---
+++ YoutubeClone/Counter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeClone
+{
+    public class Counter
+    {
+        private static Counter instance;
+        public int number;
+
+        private Counter()
+        {
+            number = 0;
+        }
+
+        public static Counter GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Counter();
+            }
+            return instance;
+        }
+    }
+}
